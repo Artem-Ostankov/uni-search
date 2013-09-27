5d37e96f19dfe3adde5d094cb45f90ddeaa28224
eleniS <eleni@cliqz.com>
2013-09-27 15:56:25 +0200

    Removing using url as a key

--- indexing/index_fetcher_results.py
+++ indexing/index_fetcher_results.py
@@ -1,7 +1,8 @@
 from pyelasticsearch import ElasticSearch
-import json, urlparse
+import json as simplejson
+import urlparse
 import codecs
-
+import simplejson as json
 
 
 es = ElasticSearch('http://localhost:9200/')
@@ -9,5 +10,6 @@ data = open('../data/data_part0.jl', 'r' )
 for l in data:
 	new_l = l.rstrip()
 	json_object = json.loads(new_l)
-	es.index('example', 'pages',json_object  ,json_object['link_url'])
+	print json_object
+	#es.index('example', 'pages', json_object)
 
