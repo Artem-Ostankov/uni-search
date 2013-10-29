2e992dc5ae2ea9c08d59808474ff9a9efb8977e5
eleniS <eleni@cliqz.com>
2013-10-29 10:05:04 +0100

    Sample search using sc.launch_cluster and user with permissions to buckets

--- fabfile.py
+++ fabfile.py
@@ -68,11 +68,12 @@ cliqz.setup(
 		'primary_install': full_install,
 		# This is an arbitrary name for the ElasticSearch cluster (default for sc.* tasks)
 		'default_cluster': 'bell',
+		'buckets': ['fetcher-output'],
 	},
 	cluster = {
 		'instances' : [
-			{'zone' : 'c1', 'spot_price' : 0.24, 'ebs_size' : 33, 'num_instances' : 2},
-			{'zone' : 'c2', 'spot_price' : 0.4, 'ebs_size' : 66, 'num_instances' : 1},
+			{'price_zone' : 'c1', 'spot_price' : 0.24, 'ebs_size' : 33, 'num_instances' : 2},
+			#{'price_zone' : 'c2', 'spot_price' : 0.4, 'ebs_size' : 66, 'num_instances' : 3},
 		]
 	},
 )
